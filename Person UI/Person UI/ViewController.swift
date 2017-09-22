5bd4dec60ea6707d44f84519a6624fd5c5031d0e
mpmmc6 <mpmmc6@mail.missouri.edu>
2017-09-22 13:09:18 -0500

    Add Person UI files

---
+++ Person UI/Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Matthew McCarthy on 9/21/17.
+//  Copyright © 2017 tech innovator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
